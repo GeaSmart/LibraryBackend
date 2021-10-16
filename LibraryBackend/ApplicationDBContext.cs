25205c37a31a1ed6c9526e784dbb660f2a00d006
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-16 13:24:44 -0500

    Initial commit

---
+++ LibraryBackend/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using LibraryBackend.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryBackend
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+
+    }
+}
