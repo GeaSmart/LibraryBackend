103ee64af1885f3df7ea4fdaab087dd6de9e31d5
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-10-20 00:51:10 -0500

    updating libro controller

--- LibraryBackend/Controllers/LibrosController.cs
+++ LibraryBackend/Controllers/LibrosController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryBackend.DTO;
+﻿using AutoMapper;
+using LibraryBackend.DTO;
 using LibraryBackend.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +15,12 @@ namespace LibraryBackend.Controllers
     public class LibrosController : ControllerBase
     {
         private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
 
-        public LibrosController(ApplicationDBContext context)
+        public LibrosController(ApplicationDBContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         [HttpGet]
@@ -39,16 +42,27 @@ namespace LibraryBackend.Controllers
         [HttpPost]
         public async Task<ActionResult> Post(LibroCreacionDTO libroCreacionDTO)
         {
+            if (libroCreacionDTO.AutoresIds == null)
+                return BadRequest("No se envió ningún autor");
+            
             var autoresIdsExistentes = await (from l in context.Autores where libroCreacionDTO.AutoresIds.Contains(l.Id) select l.Id).ToListAsync();
 
-            if(autoresIdsExistentes.Count != libroCreacionDTO.AutoresIds.Count)
-            {
-                return BadRequest("Algunos ids de autores no existen");
-            }
-            //await context.Libros.AddAsync(libro);
-            //await context.SaveChangesAsync();
-            return Ok();
+            if (autoresIdsExistentes.Count != libroCreacionDTO.AutoresIds.Count)
+                return BadRequest("Algunos ids de autores no existen");            
 
+            var libro = mapper.Map<Libro>(libroCreacionDTO);
+
+            if(libro.AutoresLibros != null)
+            {
+                for(int i = 0; i < libro.AutoresLibros.Count; i++)
+                {
+                    libro.AutoresLibros[i].Orden = i;
+                }
+            }
+
+            await context.Libros.AddAsync(libro);
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
--- LibraryBackend/Utils/AutomapperProfile.cs
+++ LibraryBackend/Utils/AutomapperProfile.cs
@@ -12,7 +12,23 @@ namespace LibraryBackend.Utils
     {
         public AutomapperProfile()
         {
-            CreateMap<LibroCreacionDTO, Libro>();
+            CreateMap<LibroCreacionDTO, Libro>()
+                .ForMember(x => x.AutoresLibros, options => options.MapFrom(MapAutoresLibros));
+        }
+
+        private List<AutorLibro> MapAutoresLibros(LibroCreacionDTO libroCreacionDTO, Libro libro)
+        {
+            var resultado = new List<AutorLibro>();
+
+            if (libroCreacionDTO.AutoresIds == null)
+                return resultado;
+
+            foreach(int id in libroCreacionDTO.AutoresIds)
+            {
+                resultado.Add(new AutorLibro { AutorId = id });
+            }
+
+            return resultado;
         }
     }
 }
